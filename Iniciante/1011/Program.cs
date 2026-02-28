33da3424710a077f2a81dc50c1e68b110db8e342
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:13:06 -0300

    Finalizado

---
+++ Iniciante/1011/Program.cs
@@ -0,0 +1,9 @@
+﻿static void Main(string[] args) { 
+
+        float r = float.Parse(Console.ReadLine());
+        double pi = 3.14159;
+        
+        double volume = (4/3.0) * pi * r * r * r;
+        
+        Console.WriteLine("VOLUME = {0:F3}", volume);
+}
