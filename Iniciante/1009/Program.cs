92158c37c1e284c54a5902e082f1202786917eda
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:09:53 -0300

    Finalizado

---
+++ Iniciante/1009/Program.cs
@@ -0,0 +1,10 @@
+﻿static void Main(string[] args) {
+        
+        string nome = Console.ReadLine();
+        double salario = double.Parse(Console.ReadLine());
+        double vendas = double.Parse(Console.ReadLine());
+        
+        double salariofinal = (vendas * 0.15) + salario;
+        
+        Console.WriteLine("TOTAL = R$ {0:F2}", salariofinal);
+}
