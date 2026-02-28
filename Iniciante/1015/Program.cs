216d87189bb5078e19ff0ff217c57bf9808837f1
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:38:57 -0300

    Finalizado

---
+++ Iniciante/1015/Program.cs
@@ -0,0 +1,23 @@
+﻿static void Main(string[] args) {
+        
+        string[] linha1 = Console.ReadLine().Split(' ');
+        string[] linha2 = Console.ReadLine().Split(' ');
+        
+        double x1 = double.Parse(linha1[0]);
+        double y1 = double.Parse(linha1[1]);
+        
+        double x2 = double.Parse(linha2[0]);
+        double y2 = double.Parse(linha2[1]);
+        
+        double xtotal = x2 - x1;
+        double ytotal = y2 - y1;
+        
+        double xquadrado = xtotal * xtotal;
+        double yquadrado = ytotal * ytotal;
+        
+        double total = xquadrado + yquadrado;
+        
+        double distancia = Math.Sqrt(total);
+        
+        Console.WriteLine(distancia.ToString("F4"));
+}
