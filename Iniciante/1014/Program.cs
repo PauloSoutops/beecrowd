54123cc01931f2dd9b7a70b02f8e6ff3518817c1
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:37:14 -0300

    Finalizado

---
+++ Iniciante/1014/Program.cs
@@ -0,0 +1,10 @@
+﻿static void Main(string[] args) { 
+        
+        int X = int.Parse(Console.ReadLine());
+        double Y = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+        double consumo = X / Y;
+
+        Console.WriteLine($"{consumo:F3} km/l");
+
+}
