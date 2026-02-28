a06cb81432ab68e258136b603a971b44700b31d8
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 15:48:03 -0300

    Finalizado

---
+++ Iniciante/1005/Program.cs
@@ -0,0 +1,9 @@
+﻿static void Main(string[] args) { 
+        
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double media = (((a * 3.5) + (b * 7.5)) / 11);
+        
+        Console.WriteLine("MEDIA = {0:F5}" , media);
+
+}
