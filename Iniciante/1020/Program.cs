2136916e062c51d2d9076059f86beec909214bc4
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:51:30 -0300

    Finalizado

---
+++ Iniciante/1020/Program.cs
@@ -0,0 +1,13 @@
+﻿static void Main(string[] args) { 
+
+        int valor = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"{valor / 365} ano(s)");
+        valor %= 365;
+        
+        Console.WriteLine($"{valor / 30} mes(es)");
+        valor %= 30;
+        
+        Console.WriteLine($"{valor / 1} dia(s)");
+        valor = valor;
+}
