e2cf1c59da6d1017bfdf4124e8220537ea1a991c
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 11:10:29 -0300

    Finalizado

---
+++ Iniciante/1003/Program.cs
@@ -0,0 +1,9 @@
+﻿static void Main(string[] args) { 
+
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int soma = a + b;
+    
+    Console.WriteLine("SOMA = " + soma);
+
+}
