b892f3bfb706f6056b8273a5760451f4076f67f9
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 11:07:08 -0300

    Finalizado

---
+++ Iniciante/1002/Program.cs
@@ -0,0 +1,9 @@
+﻿    static void Main(string[] args) { 
+
+        double raio = double.Parse(Console.ReadLine());
+        double n = 3.14159;
+        double area = n * raio * raio;
+        
+        Console.WriteLine("A={0:F4}", area);
+        
+}
