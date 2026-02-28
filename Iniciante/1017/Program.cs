d1f7c7f2286c1743ff65a66d3496a609c9730b5d
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:43:58 -0300

    Finalizado

---
+++ Iniciante/1017/Program.cs
@@ -0,0 +1,11 @@
+﻿static void Main(string[] args) {
+        
+        int tempo = int.Parse(Console.ReadLine());
+        int velocidade = int.Parse(Console.ReadLine());
+        
+        int distancia = tempo * velocidade;
+        
+        double litros = distancia / 12.0;
+        
+        Console.WriteLine(litros.ToString("F3"));
+}
