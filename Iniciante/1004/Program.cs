1a0ab7dd147f41192a809efc504acfb00f392e4d
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 15:41:41 -0300

    Finalizado

---
+++ Iniciante/1004/Program.cs
@@ -0,0 +1,10 @@
+﻿static void Main(string[] args) {
+    
+    int a = int.Parse(Console.ReadLine());
+    
+    int b = int.Parse(Console.ReadLine());
+    
+    int prod = a * b;
+    
+    Console.WriteLine("PROD = " + prod);
+}
