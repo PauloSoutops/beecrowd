7e23cf2813a68730a76b7f6a115889fdf7475a65
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:08:18 -0300

    Finalizado

---
+++ Iniciante/1008/Program.cs
@@ -0,0 +1,12 @@
+﻿static void Main(string[] args) { 
+
+    int numero = int.Parse(Console.ReadLine());
+    double horas = int.Parse(Console.ReadLine());
+    double salariohora = double.Parse(Console.ReadLine());
+    
+    double salario = salariohora * horas;
+    
+    Console.WriteLine("NUMBER = " + numero);
+    Console.WriteLine("SALARY = U$ {0:F2}", salario);
+
+}
