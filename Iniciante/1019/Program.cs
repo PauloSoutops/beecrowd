0e752417f2b54c272cf1e4c7d3c0a5780025ca34
Paulo Souto <paulosouto30@gmail.com>
2026-02-28 16:46:48 -0300

    Finalizado

---
+++ Iniciante/1019/Program.cs
@@ -0,0 +1,14 @@
+﻿static void Main(string[] args) { 
+
+        int n = int.Parse(Console.ReadLine());
+        
+        int horas = n / 3600;
+        n %= 3600;
+        
+        int minutos = n / 60;
+        n %= 60;
+        
+        int segundo = n;
+        
+        Console.WriteLine($"{horas}:{minutos}:{segundo}");
+}
