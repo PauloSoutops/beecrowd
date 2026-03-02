e9ee5d29198b18432d3007bf3c7fd0b1aa3f954e
Paulo Souto <paulosouto30@gmail.com>
2026-03-02 14:06:25 -0300

    Finalizado

---
+++ Iniciante/1043/Program.cs
@@ -0,0 +1,17 @@
+﻿static void Main(string[] args) { 
+
+        string[] ABC = Console.ReadLine().Split(' ');
+        
+        double A = double.Parse(ABC[0]);
+        double B = double.Parse(ABC[1]);
+        double C = double.Parse(ABC[2]);
+        
+        if(A+B>C && B+C>A && A+C>B){
+            double perimetro = A+B+C;
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F1"));
+        }else{
+            double area = ((A+B)*C)/2;
+            Console.WriteLine("Area = " + area.ToString("F1"));
+        }
+        
+}
