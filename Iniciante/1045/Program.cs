422f4067ec7036643bb4a23bbf85fcd4110d581b
Paulo Souto <paulosouto30@gmail.com>
2026-03-02 14:08:02 -0300

    Finalizado

---
+++ Iniciante/1045/Program.cs
@@ -0,0 +1,35 @@
+﻿static void Main(string[] args) {
+
+        double[] v = Console.ReadLine().Split(' ')
+                        .Select(double.Parse)
+                        .OrderByDescending(x => x)
+                        .ToArray();
+
+        double A = v[0];
+        double B = v[1];
+        double C = v[2];
+
+        double AQ = A * A;
+        double BQ = B * B;
+        double CQ = C * C;
+
+        if (A >= B + C) {
+            Console.WriteLine("NAO FORMA TRIANGULO");
+        }
+        else {
+            if (AQ == BQ + CQ)
+                Console.WriteLine("TRIANGULO RETANGULO");
+
+            if (AQ > BQ + CQ)
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+
+            if (AQ < BQ + CQ)
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+
+            if (A == B && B == C)
+                Console.WriteLine("TRIANGULO EQUILATERO");
+
+            else if (A == B || A == C || B == C)
+                Console.WriteLine("TRIANGULO ISOSCELES");
+        }
+}
