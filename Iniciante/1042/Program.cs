c4103670e3c02c6c394e372d820dee1022b2429e
Paulo Souto <paulosouto30@gmail.com>
2026-03-02 14:03:15 -0300

    Finalizado

---
+++ Iniciante/1042/Program.cs
@@ -0,0 +1,44 @@
+﻿static void Main(string[] args) { 
+            string[] entrada = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(entrada[0]);
+        int b = int.Parse(entrada[1]);
+        int c = int.Parse(entrada[2]);
+
+        int originalA = a;
+        int originalB = b;
+        int originalC = c;
+
+        int temp;
+
+        if (a > b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a > c)
+        {
+            temp = a;
+            a = c;
+            c = temp;
+        }
+
+        if (b > c)
+        {
+            temp = b;
+            b = c;
+            c = temp;
+        }
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine();
+        Console.WriteLine(originalA);
+        Console.WriteLine(originalB);
+        Console.WriteLine(originalC);
+    
+
+}
