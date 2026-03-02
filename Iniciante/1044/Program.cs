35417f9a2fc1827b686297944b630b4bdea7f110
Paulo Souto <paulosouto30@gmail.com>
2026-03-02 14:07:14 -0300

    Finalizado

---
+++ Iniciante/1044/Program.cs
@@ -0,0 +1,13 @@
+﻿static void Main(string[] args) { 
+
+        string[] AB = Console.ReadLine().Split(' ');
+        
+        double A = double.Parse(AB[0]);
+        double B = double.Parse(AB[1]);
+        
+        if(A % B == 0 || B % A == 0){
+            Console.WriteLine("Sao Multiplos");
+        }else{
+            Console.WriteLine("Nao sao Multiplos");
+        }
+}
