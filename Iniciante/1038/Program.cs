4b64619b6ec7620741a7760311dbd6de4f26155c
Paulo Souto <paulosouto30@gmail.com>
2026-03-02 13:57:50 -0300

    Finalizado

---
+++ Iniciante/1038/Program.cs
@@ -0,0 +1,20 @@
+﻿static void Main(string[] args) { 
+       string[] valores = Console.ReadLine().Split(' ');
+        int codigo = int.Parse(valores[0]);
+        int quantidade = int.Parse(valores[1]);
+
+        double total = 0;
+
+        if (codigo == 1)
+            total = 4.00 * quantidade;
+        else if (codigo == 2)
+            total = 4.50 * quantidade;
+        else if (codigo == 3)
+            total = 5.00 * quantidade;
+        else if (codigo == 4)
+            total = 2.00 * quantidade;
+        else if (codigo == 5)
+            total = 1.50 * quantidade;
+
+        Console.WriteLine($"Total: R$ {total:F2}");
+}
